916afabab0b6b38b8678ebe89d62fb699a28e012
Per Juchtmans <per.juchtmans@digipolis.gent>
2018-05-04 10:53:10 +0200

    Add unit tests.

---
+++ tests/ApiTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Trump;
+
+use PHPUnit\Framework\TestCase;
+use Trump\Api;
+
+
+/**
+ * @coversDefaultClass \Trump\Api
+ * @covers ::<!public>
+ */
+class ApiTest extends TestCase
+{
+
+    /** @var \Trump\Api */
+    private $api;
+
+    public function setUp()
+    {
+        $this->api = new Api();
+    }
+
+    public function tearDown() : void
+    {
+        $this->api = null;
+    }
+
+    public function testGetRandomQuote() : void
+    {
+        $quote = $this->api->getRandomQuote();
+        $this->assertInternalType('string', $quote);
+        $this->assertNotEmpty($quote);
+    }
+
+    public function testGetPersonalizedQuote() : void
+    {
+        $this->assertContains('Per', $this->api->getPersonalizedQuote('Per'));
+    }
+
+    public function testGetAllRandomQuotesReturnsAcoupleOfQuotes() : void
+    {
+        $this->assertTrue(count($this->api->getAllRandomQuotes()) > 2);
+    }
+}
